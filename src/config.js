1e16e557d09d308c0579c387c518953afd0b4b93
Ömer Ali Uygun <112541841+omerali1996@users.noreply.github.com>
2025-11-26 12:04:01 +0300

    Update config.js

--- src/config.js
+++ src/config.js
@@ -1,4 +1,5 @@
-export const API_BASE_URL = "https://convince-app-v3.onrender.com"; // Vercel deploy sonrası uygun URL ile değiştir
+export const API_BASE_URL = "https://convince-app-v4.onrender.com"; // Vercel deploy sonrası uygun URL ile değiştir
+
 
 
 
